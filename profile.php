2ade11df195ac6c0ca85c97ecf19d9e25ebbf3dc
charu510 <charu18.anant@gmail.com>
2021-10-17 13:04:21 +0530

    New changes made

--- profile.php
+++ profile.php
@@ -103,7 +103,7 @@
               else
               {
                 //no data
-                echo "No admins in database. Add admins to display them here.";
+                echo "No admins in database. Add admins ";
               }
             }
 
